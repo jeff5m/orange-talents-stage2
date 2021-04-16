339f4505a1395304fdf9d267ae31361b6ee2a8b3
jeff5m <jeffmatheuspereira@gmail.com>
2021-04-15 21:45:00 -0300

    create User entity

---
+++ src/main/java/com/orangetalents/challenge/model/domain/User.java
@@ -0,0 +1,98 @@
+package com.orangetalents.challenge.model.domain;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@SuppressWarnings({"squid:S1133", "squid:S1123", "squid:S1186"})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 120)
+    private String name;
+    @Column(nullable = false, length = 60, unique = true)
+    private String email;
+    @Column(nullable = false, length = 11, unique = true)
+    private String cpf;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Deprecated
+    public User() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!name.equals(user.name)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!cpf.equals(user.cpf)) return false;
+        return birthDate.equals(user.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + cpf.hashCode();
+        result = 31 * result + birthDate.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
